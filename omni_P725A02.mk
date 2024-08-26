a74bc9068bec866d4ecf72d1ed1bef5a6131cb0e
qingtianyx <102300603+qingtianyx@users.noreply.github.com>
2024-08-26 15:57:10 +0800

    Update omni_P725A02.mk

--- omni_P725A02.mk
+++ omni_P725A02.mk
@@ -9,8 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+
 
 # Inherit from P725A02 device
 $(call inherit-product, device/zte/P725A02/device.mk)
